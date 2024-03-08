84f7285c63cd24b0073162227a3fcdf661719413
riordan.santos <riordan3607@gmail.com>
2024-03-08 20:00:39 -0300

    feat: new technologies

--- src/components/LogoBar/LogoBar.js
+++ src/components/LogoBar/LogoBar.js
@@ -1,90 +1,88 @@
-import React from "react";
-import { Container } from "./styles";
+import React from 'react'
+import { Container } from './styles'
 
 const LogoBar = () => {
   return (
     <Container>
-      <div className="logo-item">
+      <div className='logo-item'>
         <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
-          height="60px"
-          width="70px"
-          alt="html-logo"
-          className="pulse"
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+          height='60px'
+          width='70px'
+          alt='js-logo'
+          className='pulse'
         />
       </div>
 
-      <div className="logo-item">
+      <div className='logo-item'>
         <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg"
-          height="60px"
-          width="70px"
-          alt="css-logo"
-          className="pulse"
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg'
+          height='60px'
+          width='70px'
+          alt='typescript-logo'
+          className='pulse'
         />
       </div>
 
-      <div className="logo-item">
+      <div className='logo-item'>
         <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-          height="60px"
-          width="70px"
-          alt="js-logo"
-          className="pulse"
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg'
+          height='60px'
+          width='70px'
+          alt='react-logo'
+          className='pulse react'
         />
       </div>
 
-      <div className="logo-item">
+      <div className='logo-item'>
         <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
-          height="60px"
-          width="70px"
-          alt="typescript-logo"
-          className="pulse"
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg'
+          width='70px'
+          height='60px'
+          alt='next-logo'
+          className='pulse'
         />
       </div>
 
-      <div className="logo-item">
+      <div className='logo-item'>
         <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
-          height="60px"
-          width="70px"
-          alt="react-logo"
-          className="pulse react"
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg'
+          height='60px'
+          width='70px'
+          alt='sass-logo'
+          className='pulse'
         />
       </div>
 
-      <div className="logo-item">
+      <div className='logo-item'>
         <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
-          width="70px"
-          height="60px"
-          alt="next-logo"
-          className="pulse"
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg'
+          width='70px'
+          height='60px'
+          alt='tailwind-logo'
+          className='pulse'
         />
       </div>
-
-      <div className="logo-item">
+      <div className='logo-item'>
         <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
-          height="60px"
-          width="70px"
-          alt="sass-logo"
-          className="pulse"
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nestjs/nestjs-original-wordmark.svg'
+          height='60px'
+          width='70px'
+          alt='css-logo'
+          className='pulse'
         />
       </div>
-
-      <div className="logo-item">
+      <div className='logo-item'>
         <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg"
-          width="70px"
-          height="60px"
-          alt="tailwind-logo"
-          className="pulse"
+          src='https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg'
+          height='60px'
+          width='70px'
+          alt='html-logo'
+          className='pulse'
         />
       </div>
     </Container>
-  );
-};
+  )
+}
 
-export default LogoBar;
+export default LogoBar
